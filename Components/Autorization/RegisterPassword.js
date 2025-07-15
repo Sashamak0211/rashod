119a6ac04624b4802259a4251dd9e53efeb8549a
Sashamak0211 <mak12121212@gmail.com>
2025-07-15 18:56:27 +0300

    add all

--- Components/Autorization/RegisterPassword.js
+++ Components/Autorization/RegisterPassword.js
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export function RegisterPassword() {
+
+  const [password, setPassword] = useState('')
+  const onHandleClick = () => {
+    setPassword(password)
+  }
+
   return (
     <input 
     type="password"
-     />
+    value={setPassword}
+     onClick={onHandleClick}/>
   )
 }
